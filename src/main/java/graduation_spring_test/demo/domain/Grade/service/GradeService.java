f18bebe4407c08a596aa3fc1a5369ebcadba8a8a
heejjinkim <06.hjhj.12@gmail.com>
2023-05-08 15:35:02 +0900

    주석 커밋 테스트

--- src/main/java/graduation_spring_test/demo/domain/Grade/service/GradeService.java
+++ src/main/java/graduation_spring_test/demo/domain/Grade/service/GradeService.java
@@ -1,4 +1,17 @@
 package graduation_spring_test.demo.domain.Grade.service;
 
 public interface GradeService {
+
+    //성적 입력
+
+    //특정 과목 성적 조회
+
+
+    //성적 삭제 (과목 단위). when: 동일과목 재수강 시, 기존 성적 제거하고 새로운 성적 insert 해야함
+
+    //성적 삭제 (user 단위). when: 회원 탈퇴 시
+
+
+    //성적 추가? 업데이트? -> 하나의 수강 과목 성적이 변경될 일이 없지 않나?
+    //특정 과목 수강 여부 조회?
 }
