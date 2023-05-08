dfe64e056b1a8f496b9c96adf6f18c48bb988b92
heejjinkim <06.hjhj.12@gmail.com>
2023-05-08 23:44:41 +0900

    feat: GradeDAOImpl 생성

---
+++ src/main/java/graduation_spring_test/demo/DAO/GradeDAOImpl.java
@@ -0,0 +1,23 @@
+package graduation_spring_test.demo.DAO;
+
+import graduation_spring_test.demo.domain.Grade.Grade;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GradeDAOImpl implements GradeDAO{
+
+    @Override
+    public void addGrade(Grade grade) {
+
+    }
+
+    @Override
+    public void deleteGrade(Grade grade) {
+
+    }
+
+    @Override
+    public void getGrade(String cNum) {
+
+    }
+}
