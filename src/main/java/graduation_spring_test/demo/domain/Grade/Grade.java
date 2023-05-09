59ff65a22d7948a780caa0bb8b679031a1ed1a6f
heejjinkim <06.hjhj.12@gmail.com>
2023-05-10 00:16:15 +0900

    Grade 수정

--- src/main/java/graduation_spring_test/demo/domain/Grade/Grade.java
+++ src/main/java/graduation_spring_test/demo/domain/Grade/Grade.java
@@ -1,11 +1,31 @@
 package graduation_spring_test.demo.domain.Grade;
 
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
 
-@Data
-public class Grade {
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userselectlist")
+public final class Grade {
+
+    @Id
+    @Column(name = "UserID", nullable = false)
     private String memberId;
+
+    @Id
+    @Column(name = "TNumber", nullable = false)
     private String termNum;
+
+    @Id
+    @Column(name = "CNumber", nullable = false)
     private String classNum;
+
+    @Column(name = "ClassScore", nullable = false)
     private String score;
 }
--- src/main/java/graduation_spring_test/demo/domain/Grade/controller/GradeController.java
+++ src/main/java/graduation_spring_test/demo/domain/Grade/controller/GradeController.java
@@ -1,4 +1,13 @@
 package graduation_spring_test.demo.domain.Grade.controller;
 
+import graduation_spring_test.demo.domain.Grade.service.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GradeController {
+
+    @Autowired
+    private GradeService gradeService;
 }
