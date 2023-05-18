def7a3e97eb9d62d24f66e97da7c281f36ca6798
pletain <pletain@dgu.ac.kr>
2023-05-18 12:57:47 +0900

    add graduation related DTO

---
+++ Backend/src/main/java/graduationProject/graduation_judge/DTO/GraduationResultList.java
@@ -0,0 +1,25 @@
+package graduationProject.graduation_judge.DTO;
+
+import lombok.Data;
+
+@Data
+public class GraduationResultList {
+    /**
+     * BSMCredit
+     * BSMMathCredit
+     * BSMSciCredit
+     * CommonClassCredit
+     * Course
+     * EngClassCount
+     * EngLevel
+     * EngScore
+     * GibonSoyangCredit
+     * MajorCredit
+     * Register
+     * Result
+     * SpecialMajorCredit
+     * StudentNumber
+     * TotalCredit
+     * TotalScore
+     */
+}
