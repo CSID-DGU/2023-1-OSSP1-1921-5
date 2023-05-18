6052c4386bf19821f791424fb15994ddd058bf90
heejjinkim <06.hjhj.12@gmail.com>
2023-05-18 11:45:59 +0900

    feat: GradeService 구현

--- Backend/src/main/java/graudationProject/graduation_judge/domain/Grade/service/GradeServiceImpl.java
+++ Backend/src/main/java/graudationProject/graduation_judge/domain/Grade/service/GradeServiceImpl.java
@@ -1,4 +1,31 @@
 package graudationProject.graduation_judge.domain.Grade.service;
 
-public class GradeServiceImpl {
+import graudationProject.graduation_judge.domain.Grade.Grade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GradeServiceImpl implements GradeService{
+
+    //@Autowired
+    //private GradeDao gradeDao;
+
+
+    @Override
+    public void inputGrade(Grade grade) {
+    }
+
+    @Override
+    public String getGradeByLec(String cNum) {
+        return null;
+    }
+
+    @Override
+    public void deleteGradeByLec(String cNum) {
+    }
+
+    @Override
+    public void deleteGradeByMember(String memberId) {
+
+    }
 }
