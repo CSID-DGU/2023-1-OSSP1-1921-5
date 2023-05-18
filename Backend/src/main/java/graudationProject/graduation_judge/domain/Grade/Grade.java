0f116ee1cc6eae877ea48c72fe3bb671a047f4d5
heejjinkim <06.hjhj.12@gmail.com>
2023-05-18 11:32:38 +0900

    feat: Create Grade class

---
+++ Backend/src/main/java/graudationProject/graduation_judge/domain/Grade/Grade.java
@@ -0,0 +1,10 @@
+package graudationProject.graduation_judge.domain.Grade;
+import lombok.Data;
+
+@Data
+public  class Grade {
+    private String memberId;
+    private String termNum;
+    private String classNu
+    private String score;
+}
