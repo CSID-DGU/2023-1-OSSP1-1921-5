c2d0c7b3bd40189c7ce9b3a93d672b90d48b7598
heejjinkim <06.hjhj.12@gmail.com>
2023-05-19 02:28:18 +0900

    Fix: Grade entity

--- Backend/src/main/java/graduationProject/graduation_judge/domain/Grade/Grade.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Grade/Grade.java
@@ -1,10 +1,34 @@
 package graduationProject.graduation_judge.domain.Grade;
-import lombok.Data;
 
-@Data
-public  class Grade {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userselectlist")
+public final class Grade {
+
+    @Id
+    @Column(name = "UserID", nullable = false)
     private String memberId;
+
+    @Id
+    @Column(name = "TNumber", nullable = false)
     private String termNum;
+
+    @Id
+    @Column(name = "CNumber", nullable = false)
     private String classNum;
+
+    @Column(name = "ClassScore", nullable = false)
     private String score;
 }
