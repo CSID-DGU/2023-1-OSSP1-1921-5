087f1feba757e539f41a3c072940dc5bcaa74140
heejjinkim <06.hjhj.12@gmail.com>
2023-05-19 02:01:14 +0900

    Feat: GradeDAOImpl 추가

--- Backend/src/main/java/graduationProject/graduation_judge/DAO/GradeDAO.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/GradeDAO.java
@@ -2,7 +2,7 @@ package graduationProject.graduation_judge.DAO;
 
 import graduationProject.graduation_judge.domain.Grade.Grade;
 
-public interface GradeDao {
+public interface GradeDAO {
     void addGrade(Grade grade); // 성적 추가
     void  deleteGradeByLec(String cNum); // 과목단위 성적 삭제(동일과목 재수강 시, 기존 성적 제거하고 새로운 성적 insert 필요)
     void getGrade(String cNum); // 특정 과목 성적 조회
---
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/GradeDAOImpl.java
@@ -0,0 +1,35 @@
+package graduationProject.graduation_judge.DAO;
+
+import graduationProject.graduation_judge.domain.Grade.Grade;
+
+public class GradeDAOImpl implements GradeDAO{
+    @Override
+    public void addGrade(Grade grade) {
+
+    }
+
+    @Override
+    public void deleteGradeByLec(String cNum) {
+
+    }
+
+    @Override
+    public void getGrade(String cNum) {
+
+    }
+
+    @Override
+    public void getAllScore(String memberId) {
+
+    }
+
+    @Override
+    public void getMajorScore(String memberId) {
+
+    }
+
+    @Override
+    public void getCredit(String memberId) {
+
+    }
+}
