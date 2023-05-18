6ad8428bbc90b2c5367ab7aee3401fc6d0b17d66
pletain <pletain@dgu.ac.kr>
2023-05-18 12:59:19 +0900

    add graduation service

--- Backend/src/main/java/graduationProject/graduation_judge/domain/Graduation/service/GraduationService.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Graduation/service/GraduationService.java
@@ -1,4 +1,12 @@
 package graduationProject.graduation_judge.domain.Graduation.service;
 
+import graduationProject.graduation_judge.DTO.EssLectureResult;
+import graduationProject.graduation_judge.DTO.GraduationResultList;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface GraduationService {
+    public GraduationResultList checkGraduationEligibility();
+
+    public EssLectureResult checkEssLectureCompletion();
 }
--- Backend/src/main/java/graduationProject/graduation_judge/domain/Graduation/service/GraduationServiceImpl.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Graduation/service/GraduationServiceImpl.java
@@ -1,4 +1,19 @@
 package graduationProject.graduation_judge.domain.Graduation.service;
 
-public class GraduationServiceImpl {
+import graduationProject.graduation_judge.DTO.EssLectureResult;
+import graduationProject.graduation_judge.DTO.GraduationResultList;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GraduationServiceImpl implements GraduationService{
+
+    @Override
+    public GraduationResultList checkGraduationEligibility() {
+        return null;
+    }
+
+    @Override
+    public EssLectureResult checkEssLectureCompletion() {
+        return null;
+    }
 }
