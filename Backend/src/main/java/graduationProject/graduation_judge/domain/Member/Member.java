49607ca84535323902ce8a96c5f4bf9666d44bf4
dabeann <dabin4078@daum.net>
2023-05-18 13:38:11 +0900

    fix Member

--- Backend/src/main/java/graduationProject/graduation_judge/domain/Member/Member.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Member/Member.java
@@ -2,90 +2,37 @@ package graduationProject.graduation_judge.domain.Member;
 
 import graduationProject.graduation_judge.global.common_unit.English_level;
 import graduationProject.graduation_judge.global.common_unit.Major_curriculum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-public class Member {
+@Table(name = "userinfo")
+public final class Member {
     @Id
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private String id; //아이디
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password; //비밀번호
-    @Column(name = "enroll_year")
+    @Column(name = "enroll_year", nullable = false)
     private Integer enroll_year; //입학년도
-    @Column(name = "completed_semesters")
+    @Column(name = "completed_semesters", nullable = false)
     private Integer completed_semesters; //학기
-    @Column(name = "major_curriculum")
+    @Column(name = "major_curriculum", nullable = false)
     private Major_curriculum major_curriculum; //심화 or 일반
-    @Column(name = "eng_level")
+    @Column(name = "eng_level", nullable = false)
     private English_level eng_level; //S0, S1..
-    @Column(name = "TOEIC_score")
+    @Column(name = "TOEIC_score", nullable = false)
     private Integer TOEIC_score; //토익 점수
     @Column(name = "security_code")
     private String  security_code; //비밀번호 변경시 보안코드
-
-    public Member(String id, String password, Integer enroll_year, Integer completed_semesters, Major_curriculum major_curriculum, English_level eng_level, Integer TOEIC_score, String security_code) {
-        this.id = id;
-        this.password = password;
-        this.enroll_year = enroll_year;
-        this.completed_semesters = completed_semesters;
-        this.major_curriculum = major_curriculum;
-        this.eng_level = eng_level;
-        this.TOEIC_score = TOEIC_score;
-        this.security_code = security_code;
-    }
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public Integer getEnroll_year() {
-        return enroll_year;
-    }
-    public void setEnroll_year(Integer enroll_year) {
-        this.enroll_year = enroll_year;
-    }
-    public Integer getCompleted_semesters() {
-        return completed_semesters;
-    }
-    public void setCompleted_semesters(Integer completed_semesters) {
-        this.completed_semesters = completed_semesters;
-    }
-    public Major_curriculum getMajor_curriculum() {
-        return major_curriculum;
-    }
-    public void setMajor_curriculum(Major_curriculum major_curriculum) {
-        this.major_curriculum = major_curriculum;
-    }
-    public English_level getEng_level() {
-        return eng_level;
-    }
-    public void setEng_level(English_level eng_level) {
-        this.eng_level = eng_level;
-    }
-    public Integer getTOEIC_score() {
-        return TOEIC_score;
-    }
-    public void setTOEIC_score(Integer TOEIC_score) {
-        this.TOEIC_score = TOEIC_score;
-    }
-
-    public String getSecurity_code() {
-        return security_code;
-    }
-
-    public void setSecurity_code(String security_code) {
-        this.security_code = security_code;
-    }
 }
