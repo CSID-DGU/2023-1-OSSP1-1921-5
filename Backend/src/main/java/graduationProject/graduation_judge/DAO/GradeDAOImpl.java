b7dd3fe832c522ffbd534d607e72285e6fa883b0
heejjinkim <06.hjhj.12@gmail.com>
2023-05-19 02:50:08 +0900

    Feat+Refactor: GradeDAO 특정과목수강여부 기능 추가

--- Backend/src/main/java/graduationProject/graduation_judge/DAO/GradeDAOImpl.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/GradeDAOImpl.java
@@ -1,35 +1,55 @@
 package graduationProject.graduation_judge.DAO;
 
 import graduationProject.graduation_judge.domain.Grade.Grade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+
+@Repository
 public class GradeDAOImpl implements GradeDAO{
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public GradeDAOImpl(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
     @Override
     public void addGrade(Grade grade) {
+        String sql= "INSERT INTO `UserSelectList` VALUES (?, ?, ?, ?)";
+        int result = jdbcTemplate.update(sql, grade.getMemberId(), grade.getTermNum(), grade.getClassNum(), grade.getScore());
+    }
+
+    @Override
+    public void deleteGrade(Grade grade) {
 
     }
 
     @Override
-    public void deleteGradeByLec(String cNum) {
-
+    public String getGrade(String cNum) {
+        return null;
     }
 
     @Override
-    public void getGrade(String cNum) {
-
+    public int isExistGrade(Grade grade) {
+        String sql = "select COUNT(*) AS count from UserSelectList where UserID = ? AND CNumber LIKE ?";
+        int result = jdbcTemplate.queryForObject(sql, Integer.class, grade.getMemberId(), grade.getClassNum());
+        return result;
     }
 
     @Override
-    public void getAllScore(String memberId) {
-
+    public float getSumOfAllScore(String memberId) {
+        return 0;
     }
 
     @Override
-    public void getMajorScore(String memberId) {
-
+    public float getSumOfMajorScore(String memberId) {
+        return 0;
     }
 
     @Override
-    public void getCredit(String memberId) {
-
+    public int getCredit(String memberId) {
+        return 0;
     }
 }
