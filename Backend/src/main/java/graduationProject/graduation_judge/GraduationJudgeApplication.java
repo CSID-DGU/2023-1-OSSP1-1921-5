e674db7902b539680bfc121c7da0361a510bad30
pletain <pletain@dgu.ac.kr>
2023-05-22 21:02:31 +0900

    build: 개발단계에서 컴포넌트 스캔 시 특정도메인 제외 코드 추가

--- Backend/src/main/java/graduationProject/graduation_judge/GraduationJudgeApplication.java
+++ Backend/src/main/java/graduationProject/graduation_judge/GraduationJudgeApplication.java
@@ -2,8 +2,20 @@ package graduationProject.graduation_judge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+//특정 도메인 컴포넌트 스캔 제외
+//@ComponentScan(basePackages = {
+//		"graduationProject.graduation_judge",
+////		"other.package.to.include"
+//}, excludeFilters = {
+//		@ComponentScan.Filter(
+//				type = FilterType.REGEX,
+//				pattern = "graduationProject.graduation_judge.domain.*"
+//		),
+//})
 public class GraduationJudgeApplication {
 
 	public static void main(String[] args) {
