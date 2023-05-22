4e28f3eeb8566722a013970cb9580c869df1712c
heejjinkim <06.hjhj.12@gmail.com>
2023-05-23 01:55:50 +0900

    Fetch: InfoLecture entity 생성

--- Backend/src/main/java/graduationProject/graduation_judge/DAO/EntireLecture.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/EntireLecture.java
@@ -1,7 +1,5 @@
 package graduationProject.graduation_judge.DAO;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import graduationProject.graduation_judge.domain.Grade.GradeId;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,19 +9,18 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @SuperBuilder
 @Getter
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @NoArgsConstructor
 @AllArgsConstructor
-@IdClass(GradeId.class)
 @Table(name = "entire_lecture")
 public class EntireLecture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "TermNumber ", nullable = false)
+    @Column(name = "TermNumber ", length = 200, nullable = false)
     private String termNum;
 
     @Id
-    @Column(name = "ClassNumber", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ClassNumber", length = 200, nullable = false)
     private String classNum;
 
     @Column(name = "ClassNumber")
---
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/InfoLecture.java
@@ -0,0 +1,36 @@
+package graduationProject.graduation_judge.DAO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import graduationProject.graduation_judge.domain.Grade.GradeId;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "info_lecture")
+public class InfoLecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Info_ClassNumber ", length = 200, nullable = false)
+    private String classNum;
+
+    @Column(name = "LectureNick ", length = 200, nullable = false)
+    private String lectureNick;
+
+    @Column(name = "Curriculum", length = 200, nullable = false)
+    private String curriculum;
+
+    @Column(name = "Curriculum", length = 200)
+    private String classArea;
+
+    @Column(name = "Curriculum", nullable = false)
+    private int classCredit;
+
+
+}
