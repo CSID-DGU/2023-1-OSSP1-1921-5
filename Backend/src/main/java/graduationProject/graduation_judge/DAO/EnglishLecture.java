be7279d3a69e0eff5ada68c2f0d73b1ef3f27e4c
heejjinkim <06.hjhj.12@gmail.com>
2023-05-23 02:15:22 +0900

    Fetch: EnglishLecture entity 생성

---
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/EnglishLecture.java
@@ -0,0 +1,32 @@
+package graduationProject.graduation_judge.DAO;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "english_lecture")
+public class EnglishLecture {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "English_TermNumber", length = 200, nullable = false)
+    private String termNum;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "English_ClassNumber", length = 200, nullable = false)
+    private String classNum;
+
+    @OneToOne
+    @JoinColumn(name = "termNum", referencedColumnName = "TermNumber")
+    @JoinColumn(name = "classNum", referencedColumnName = "ClassNumber")
+    private EntireLecture entireLecture;
+}
