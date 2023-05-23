2c5ec9d56163a1415483d01e57a3bd9cbc40ef37
dabeann <dabin4078@daum.net>
2023-05-23 16:12:11 +0900

    fix: 변경된 UserInfo에 따라 MemberRepository수정

--- Backend/src/main/java/graduationProject/graduation_judge/domain/Member/repository/MemberRepository.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Member/repository/MemberRepository.java
@@ -1,7 +1,8 @@
 package graduationProject.graduation_judge.domain.Member.repository;
 
+import graduationProject.graduation_judge.DAO.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findById(String id);
+public interface MemberRepository extends JpaRepository<UserInfo, Long> {
+    UserInfo findById(String id);
 }
