fd9adbbdecbf409ce2312eabbfa437a52d6ca2e1
heejjinkim <06.hjhj.12@gmail.com>
2023-05-23 17:46:22 +0900

    Fix: GradeId

--- Backend/src/main/java/graduationProject/graduation_judge/DAO/UserSelectList.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DAO/UserSelectList.java
@@ -1,5 +1,6 @@
 package graduationProject.graduation_judge.DAO;
 
+import graduationProject.graduation_judge.domain.Grade.GradeId;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@IdClass(GradeId.class)
 @Table(name = "UserSelectList")
 public class UserSelectList {
 
--- Backend/src/main/java/graduationProject/graduation_judge/domain/Grade/GradeId.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Grade/GradeId.java
@@ -1,11 +1,22 @@
 package graduationProject.graduation_judge.domain.Grade;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class GradeId implements Serializable {
 
     private String memberId;
+
     private String termNum;
+
     private String classNum;
 
 }
