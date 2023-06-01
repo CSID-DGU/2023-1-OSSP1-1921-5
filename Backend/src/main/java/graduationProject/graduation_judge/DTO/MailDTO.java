d26f83d6e0faef8f528f97d0de7f827ff1246774
dabeann <dabin4078@daum.net>
2023-06-01 11:16:27 +0900

    delete: entity의 @getter, @setter

--- Backend/src/main/java/graduationProject/graduation_judge/DTO/MailDTO.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DTO/MailDTO.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @Data
 public class MailDTO {
--- Backend/src/main/java/graduationProject/graduation_judge/DTO/UserInfoDTO.java
+++ Backend/src/main/java/graduationProject/graduation_judge/DTO/UserInfoDTO.java
@@ -11,8 +11,6 @@ import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
 @Data
 @AllArgsConstructor
 public class UserInfoDTO {
