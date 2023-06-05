06789fadf87654d406c0a51c478b1bfe7df504fa
dabeann <dabin4078@daum.net>
2023-06-05 22:22:07 +0900

    add: lectureService

--- Backend/src/main/java/graduationProject/graduation_judge/domain/Lecture/controller/LectureController.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/Lecture/controller/LectureController.java
@@ -2,7 +2,9 @@ package graduationProject.graduation_judge.domain.Lecture.controller;
 
 
 import graduationProject.graduation_judge.DTO.Lecture.GetLectureInfo.GetLectureInfoIncludeSemesterDTO;
+import graduationProject.graduation_judge.domain.Lecture.Service.LectureService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +17,15 @@ import java.util.HashMap;
 @RequestMapping("/adminpage")
 public class LectureController {
 
+    @Autowired
+    private LectureService lectureService;
 
     @PostMapping("/uploadNewSemester")
     public ResponseEntity<?> uploadNewSemester(@RequestBody GetLectureInfoIncludeSemesterDTO getLectureDTO){
 
         try{
-            
-            return ResponseEntity.ok().body(getLectureDTO);
+            lectureService.inputLecture(getLectureDTO);
+            return ResponseEntity.ok().build();
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e);
         }
