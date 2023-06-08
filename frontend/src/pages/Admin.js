18b72747514fbfcd6859bd875f8b0a4552ff417e
Sj0-0i <sjy140801@dgu.ac.kr>
2023-06-08 21:08:06 +0900

    feat: 회원목록

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink as RouterLink, Route, Routes } from 'react-router-dom';
 import CreateDataSet from "./CreateDataSet"
 import Header from '../Components/Header';
@@ -6,19 +6,43 @@ import './css/Admin.css';
 import SideBar from '../Components/SideBar';
 
 const Admin = () => {
+
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        fetchMembers();
+    }, []);
+
+    const fetchMembers = async () => {
+        try {
+            const response = await fetch('/showUserInfoList');
+            const data = await response.json();
+            setMembers(data);
+        } catch (error) {
+            console.error('Error', error);
+        }
+    }
+
+
     return (
         <div className="fade-in">
-        <Header signout />
-        {/* <MenuBar /> */}
-        <SideBar />
-        <div className="container">
-            <Routes>
-                <Route path="/admin" exact={true} />
-                <Route path="/change" element={<CreateDataSet />} />
-                <Route path="/newsem" element={<CreateDataSet />} />
-                <Route path="/dataset" element={<CreateDataSet />} />
-            </Routes>
-        </div>
+            <Header signout />
+            {/* <MenuBar /> */}
+            <SideBar />
+            <div className="container">
+                <Routes>
+                    <Route path="/admin" exact={true} />
+                    <Route path="/change" element={<CreateDataSet />} />
+                    <Route path="/newsem" element={<CreateDataSet />} />
+                    <Route path="/dataset" element={<CreateDataSet />} />
+                </Routes>
+                <h1>회원 목록</h1>
+                <ul>
+                    {members.map((member) => (
+                        <li key={member.id}>{member.name}</li> 
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
