85d5f63dc1b8e32d268a0f9269d6931461180bd8
heejjinkim <06.hjhj.12@gmail.com>
2023-06-11 23:11:12 +0900

    fix: DataSetController 구성 수정

--- Backend/src/main/java/graduationProject/graduation_judge/domain/DataSet/controller/DataSetController.java
+++ Backend/src/main/java/graduationProject/graduation_judge/domain/DataSet/controller/DataSetController.java
@@ -5,7 +5,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,14 +16,49 @@ import java.util.Map;
 @RestController
 @RequestMapping("/dataset")
 public class DataSetController {
-    private static final String FilePath = "../../../../../../../../testDataSet/data";
+
     @PostMapping("/create")
-    public ResponseEntity<?> createDataSet(@RequestBody Map<String, String> request){
-        // test data set 명령어 입력 받기
-        // testDataset python 으로 명령어 보내기
-        // 생성 완료됐다고 ok 결과 프론트로 보내기
+    public ResponseEntity<?> createDataSet(@RequestBody Map<String, String> request)throws IOException{
+        // test data set 명령어 입력 받기 (파라미터로 넘길 인자들)
+        String dataNum = request.get("dataNum"); // 만들 data 수
+        String admissionYear = request.get("admissionYear"); // 입학년도
+        String completeSem = request.get("completeSem"); // 이수학기
+        String subjects = request.get("subjects"); // 제약사항 관련 (수정 필요~)
+
         try{
-            return null;
+            // testDataset python 으로 명령어 보내기
+            String createDataPython = "/testDataSet/createData.py"; // 실행할 파이썬 스크립트의 경로
+            String functionName = ""; // 실행할 함수의 이름
+
+            // 실행할 명령어 (수정 필요~)
+            String command = String.format("python %s %s %s %s", createDataPython, functionName, dataNum, admissionYear);
+
+            // 명령어 실행
+            ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+            Process process = processBuilder.start();
+
+            // python 처리완료될 때 까지 기다리기
+            try {
+                int exitCode = process.waitFor();
+                System.out.println("Python script execution completed with exit code: " + exitCode);
+            } catch (InterruptedException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+            // exitCode 값에 따른 처리 필요?
+
+            // python으로부터 결과 받기 (test set의 저장 경로)
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // 결과 읽기
+            }
+            // test set 저장 경로를 z3에게 보내기
+            
+            // z3로부터 결과 엑셀 파일 저장경로 받기
+
+            // 생성 완료됐다고 ok 결과 프론트로 보내기
+
+            return ResponseEntity.ok().body("데이터셋 생성 완료");
 
         }catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -43,7 +79,8 @@ public class DataSetController {
 
     @GetMapping("/download")
     public void downloadFiles(HttpServletResponse response) throws IOException{
-        // 테스트셋 다운로드
+        // 테스트셋 다운로드 + z3 결과 다운로드
+        String FilePath = "../../../../../../../../testDataSet/data";
         File directory = new File(FilePath);
         File[] files = directory.listFiles();
         if (files != null) {
