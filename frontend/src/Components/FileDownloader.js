01be6a28e3a81a8528d82365f8ddc443049c666e
Sj0-0i <sjy140801@dgu.ac.kr>
2023-06-18 16:44:56 +0900

    feat: testdataset 파일 다운로드

---
+++ frontend/src/Components/FileDownloader.js
@@ -0,0 +1,22 @@
+import React from "react";
+import data from '../../testDataset/data';
+
+const FileDownloader = () => {
+    const handleDownloadAll = () => {
+        const folderPath = data; // 다운로드할 폴더 경로
+
+        const downloadLink = document.createElement("a");
+        downloadLink.href = folderPath;
+        downloadLink.download = "TestDataSet.zip";
+        downloadLink.click();
+    };
+
+    return (
+        <div>
+            <h2>Test DataSet Download</h2>
+            <button onClick={handleDownloadAll}>download</button>
+        </div>
+    );
+};
+
+export default FileDownloader;
